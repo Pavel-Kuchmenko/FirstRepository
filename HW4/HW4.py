44a0b2aa19edf31420832314e5140ad833928003
WIN-9HUFBJGI376\User <sigmakmn@gmail.com>
2021-06-11 19:22:36 +0300

    HomeWork_4_add_Prints

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -1,20 +1,24 @@
 user_data = {"name":None, "surname":None}
+#------------------------Say hello to new user---------------------------
 print("--------------------------2-d task----------------------------")
+print("------------------Say hello to new user-----------------------")
 print("Enter your name:")
 user_data["name"] = input()
 print("Enter your surname:")
 user_data["surname"] = input()
 print(f"Hi {user_data['name']} {user_data['surname']}!\n\n")
-#----------------------------------------------------------------------
+#---------------------Get summ and multiplications-----------------------
 print("--------------------------1-st task---------------------------")
+print("---------------Get summ and multiplications-------------------")
 print("Lests start.")
 print("Enter 3 integers:")
 digits_list = [0, 0, 0]
 digits_list[0], digits_list[1], digits_list[2] = int(input()), int(input()), int(input())
 print(f"{digits_list[0]} + {digits_list[1]} = {digits_list[0]+digits_list[1]}")
 print(f"{digits_list[1]} * {digits_list[2]} = {digits_list[1] * digits_list[2]}\n\n")
-#----------------------------------------------------------------------
+#---------------Convert meters to kilometers and milimeters---------------
 print("--------------------------3-rd task---------------------------")
+print("---------Convert meters to kilometers and milimeters----------")
 print(f"{user_data['name']} {user_data['surname']} welcome to distance converter!")
 distance: int = 0
 print("Enter distance in meters:")
