15e61a2ceafe3115a71b08a3f9bb92bba3068a56
WIN-9HUFBJGI376\User <sigmakmn@gmail.com>
2021-06-18 19:49:58 +0300

    HomeWork_2.4_2

--- HW2.4/HW2.4_6.py
+++ HW2.4/HW2.4_6.py
@@ -1,7 +1,20 @@
 from random import randint
 
-A = ([randint(1, 50) for a in range(15)])
-B = ([randint(1, 50) for a in range(15)])
-C = ([randint(1, 50) for a in range(15)])
+A = {1,2,3,4,5,6,7,8}
+B = {22,2,24,16,66,71,22,4}
+C = {0,2,12,17,26,22,1,24}
+print(A)
+print(B)
+print(C)
 
-print()
+print(f"Similar elements from all tuples:{A&B&C}")
+print(f"Elemts that unique for every tuple:{A^B^C}")
+print("Similar values at same positions:\t")
+
+A = {1,2,3,4,5,6,7,8}
+B = {22,2,24,16,66,71,22,4}
+C = {0,2,12,17,26,22,1,24}
+
+for value_a, value_b, value_c in zip(A,B,C):
+    if value_a == value_b == value_c:
+        print(value_a)
